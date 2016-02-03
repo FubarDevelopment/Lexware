12118e8f87df310e9546a3f135a29c364c1d54ec
Mark Junker <m.junker@fubar-dev.de>
2016-02-03 12:31:46 +0100

    Unit-Test für die Passwort-Ver- und Entschlüsselung

---
+++ FubarDev.Lexware.Database.Tests/PasswordEncryption.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="PasswordEncryption.cs" company="Fubar Development Junker">
+// Copyright (c) Fubar Development Junker. All rights reserved.
+// </copyright>
+
+using Xunit;
+
+namespace FubarDev.Lexware.Database.Tests
+{
+    public class PasswordEncryption
+    {
+        [Fact(DisplayName = "Ver- und Entschlüsselung von _login_")]
+        public void TestEncryption()
+        {
+            Assert.Equal("_login_", LexwarePassword.Decrypt("92ab346d1c02cffc"));
+            Assert.Equal("92ab346d1c02cffc", LexwarePassword.Encrypt("_login_"));
+        }
+    }
+}
---
+++ FubarDev.Lexware.Database.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Fubar Development Junker">
+// Copyright (c) Fubar Development Junker. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("FubarDev.Lexware.Database.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("FubarDev.Lexware.Database.Tests")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("de54d39e-c28b-4b01-b634-8c0e802b5b63")]
